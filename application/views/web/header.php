8ee4b4a87fc2c4e534aca1b5a837edf18dd78b5b
refiahmadfauzi <34942113+refiahmadfauzi@users.noreply.github.com>
2022-09-02 20:45:36 +0700

    Update header.php

--- application/views/web/header.php
+++ application/views/web/header.php
@@ -99,9 +99,7 @@
             <div class="row align-items-center">
               <div class="col-lg-2 col-md-2">
                 <!-- Logo -->
-                <div class="logo">
-                  <a href="<?php echo base_url() ?>"><img src="<?php echo base_url() . $s->stp_brand_icon ?>" style="max-height: 300px" alt="" /></a>
-                </div>
+                <a href="<?php echo base_url() ?>"><img src="<?php echo base_url() . $s->stp_brand_icon ?>" style="height: 50px; width:176px" alt="" /></a>
               </div>
               <div class="col-lg-10 col-md-10">
                 <div class="menu-wrapper">
