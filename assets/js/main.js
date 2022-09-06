b0715fd3caff9b82e87d6e0947e4a5c919c7f3c7
refiahmadfauzi <34942113+refiahmadfauzi@users.noreply.github.com>
2022-09-06 13:38:49 +0700

    fixed top bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,19 +172,10 @@
 	/* 7.  Custom Sticky Menu  */
 	$(window).on('scroll', function () {
 		var scroll = $(window).scrollTop();
-		if (scroll < 245) {
+		if (scroll < 50) {
 			$(".header-sticky").removeClass("sticky-bar");
 		} else {
 			$(".header-sticky").addClass("sticky-bar");
-		}
-	});
-
-	$(window).on('scroll', function () {
-		var scroll = $(window).scrollTop();
-		if (scroll < 245) {
-			$(".header-sticky").removeClass("sticky");
-		} else {
-			$(".header-sticky").addClass("sticky");
 		}
 	});
 
